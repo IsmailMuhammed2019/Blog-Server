709029068e7e811a6443552b60c01b0df0f58360
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2024-02-15 13:24:51 +0100

    update middlewear file

--- middleware.js
+++ middleware.js
@@ -30,6 +30,37 @@
 // };
 
 
+// const config = require("./config");
+
+// let checkToken = (req, res, next) => {
+//   let token = req.headers["authorization"];
+//   console.log(token);
+//   if (!token || !token.startsWith("Bearer ")) {
+//     return res.status(401).json({
+//       status: false,
+//       msg: "Token is not provided or invalid",
+//     });
+//   }
+  
+//   token = token.slice(7); // Remove 'Bearer ' from the token string
+//   try {
+//     const decoded = jwtDecode(token);
+//     req.decoded = decoded;
+//     next();
+//   } catch (error) {
+//     return res.status(401).json({
+//       status: false,
+//       msg: "Token is invalid",
+//     });
+//   }
+// };
+
+// module.exports = {
+//   checkToken: checkToken,
+// };
+
+
+
 const config = require("./config");
 
 let checkToken = (req, res, next) => {
