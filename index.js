00b99a2ac2e9a8422200bdf0c4dedc3a2bb643f2
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2024-02-15 13:39:03 +0100

    revert back

--- index.js
+++ index.js
@@ -3,131 +3,40 @@ const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
 const app = express();
 
-mongoose.connect(process.env.MONODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => {
-  console.log('mongodb connected');
-})
-.catch(error => {
-  console.error('MongoDB connection error:', error);
+mongoose.connect(
+  "mongodb+srv://admin:CloudTrack..@userdata.imbivt0.mongodb.net/",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDb connected");
 });
 
-// Middleware
+//middleware
 app.use("/uploads", express.static("uploads"));
 app.use(express.json());
 const userRoute = require("./routes/user");
-const profileRoute = require("./routes/profile");
-const blogRoute = require("./routes/blogpost");
 app.use("/user", userRoute);
+const profileRoute = require("./routes/profile");
 app.use("/profile", profileRoute);
+const blogRoute = require("./routes/blogpost");
 app.use("/blogPost", blogRoute);
 
-// Define response for the root endpoint ("/")
-app.get("/", (req, res) => {
-  const data = {
-    msg: "Welcome on DevStack Blog App development YouTube video series",
-    info: "This is a root endpoint",
-    Working: "Documentations of other endpoints will be released soon :)",
-    request: "Hey if you didn't subscribe to my YouTube channel, please subscribe to it"
-  };
-  res.json(data);
-});
+data = {
+  msg: "Welcome on DevStack Blog App development YouTube video series",
+  info: "This is a root endpoint",
+  Working: "Documentations of other endpoints will be release soon :)",
+  request:
+    "Hey if you did'nt subscribed my YouTube channle please subscribe it",
+};
+
+app.route("/").get((req, res) => res.json(data));
 
 app.listen(port, "0.0.0.0", () =>
-  console.log(`Listening on port ${port}`)
+  console.log(`welcome your listinnig at port ${port}`)
 );
-
-
-
-
-
-
-
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const port = process.env.PORT || 5000;
-// const app = express();
-
-// mongoose.connect(
-//   process.env.MONODB_URI,{useNewUrlParser: true, useUnifiedTopology : true})
-//   .then(()=>{
-//     console.log('mongodb connected')
-
-//   })
-// ;
-
-// // const connection = mongoose.connection;
-// // connection.once("open", () => {
-// //   console.log("MongoDb connected");
-// // });
-
-// //middleware
-// app.use("/uploads", express.static("uploads"));
-// app.use(express.json());
-// const userRoute = require("./routes/user");
-// app.use("/user", userRoute);
-// const profileRoute = require("./routes/profile");
-// app.use("/profile", profileRoute);
-// const blogRoute = require("./routes/blogpost");
-// app.use("/blogPost", blogRoute);
-
-// data = {
-//   msg: "Welcome on DevStack Blog App development YouTube video series",
-//   info: "This is a root endpoint",
-//   Working: "Documentations of other endpoints will be release soon :)",
-//   request:
-//     "Hey if you did'nt subscribed my YouTube channle please subscribe it",
-// };
-
-// app.route("/").get((req, res) => res.json(data));
-
-// app.listen(port, "0.0.0.0", () =>
-//   console.log(`welcome your listinnig at port ${port}`)
-// );
-
-
-
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const port = process.env.PORT || 5000;
-// const app = express();
-
-// mongoose.connect(
-//   "mongodb+srv://admin:CloudTrack..@userdata.imbivt0.mongodb.net/",
-//   {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useUnifiedTopology: true,
-//   }
-// );
-
-// const connection = mongoose.connection;
-// connection.once("open", () => {
-//   console.log("MongoDb connected");
-// });
-
-// //middleware
-// app.use("/uploads", express.static("uploads"));
-// app.use(express.json());
-// const userRoute = require("./routes/user");
-// app.use("/user", userRoute);
-// const profileRoute = require("./routes/profile");
-// app.use("/profile", profileRoute);
-// const blogRoute = require("./routes/blogpost");
-// app.use("/blogPost", blogRoute);
-
-// data = {
-//   msg: "Welcome on DevStack Blog App development YouTube video series",
-//   info: "This is a root endpoint",
-//   Working: "Documentations of other endpoints will be release soon :)",
-//   request:
-//     "Hey if you did'nt subscribed my YouTube channle please subscribe it",
-// };
-
-// app.route("/").get((req, res) => res.json(data));
-
-// app.listen(port, "0.0.0.0", () =>
-//   console.log(`welcome your listinnig at port ${port}`)
-// );
--- middleware.js
+++ middleware.js
@@ -1,87 +1,26 @@
-// const jwt = require("jsonwebtoken");
-// const config = require("./config");
-
-// let checkToken = (req, res, next) => {
-//   let token = req.headers["authorization"];
-//   console.log(token);
-//   token = token.slice(7, token.length);
-//   if (token) {
-//     jwt.verify(token, config.key, (err, decoded) => {
-//       if (err) {
-//         return res.json({
-//           status: false,
-//           msg: "token is invalid",
-//         });
-//       } else {
-//         req.decoded = decoded;
-//         next();
-//       }
-//     });
-//   } else {
-//     return res.json({
-//       status: false,
-//       msg: "Token is not provided",
-//     });
-//   }
-// };
-
-// module.exports = {
-//   checkToken: checkToken,
-// };
-
-
-// const config = require("./config");
-
-// let checkToken = (req, res, next) => {
-//   let token = req.headers["authorization"];
-//   console.log(token);
-//   if (!token || !token.startsWith("Bearer ")) {
-//     return res.status(401).json({
-//       status: false,
-//       msg: "Token is not provided or invalid",
-//     });
-//   }
-  
-//   token = token.slice(7); // Remove 'Bearer ' from the token string
-//   try {
-//     const decoded = jwtDecode(token);
-//     req.decoded = decoded;
-//     next();
-//   } catch (error) {
-//     return res.status(401).json({
-//       status: false,
-//       msg: "Token is invalid",
-//     });
-//   }
-// };
-
-// module.exports = {
-//   checkToken: checkToken,
-// };
-
-
-
+const jwt = require("jsonwebtoken");
 const config = require("./config");
 
 let checkToken = (req, res, next) => {
   let token = req.headers["authorization"];
   console.log(token);
-  if (!token || !token.startsWith("Bearer ")) {
-    return res.status(401).json({
-      status: false,
-      msg: "Token is not provided or invalid",
+  token = token.slice(7, token.length);
+  if (token) {
+    jwt.verify(token, config.key, (err, decoded) => {
+      if (err) {
+        return res.json({
+          status: false,
+          msg: "token is invalid",
+        });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
     });
-  }
-  
-  token = token.slice(7); // Remove 'Bearer ' from the token string
-  try {
-    const decoded = jwtDecode(token);
-    req.decoded = decoded;
-    next();
-  } catch (error) {
-    return res.status(401).json({
+  } else {
+    return res.json({
       status: false,
-      msg: "Token is invalid",
+      msg: "Token is not provided",
     });
   }
 };
