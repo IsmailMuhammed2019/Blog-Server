ba3d5e4a49942f9233bb37f4bffb1c9eecee8202
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2024-02-15 12:41:21 +0100

    update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,26 +1,56 @@
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
+// const config = require("./config");
+
+// let checkToken = (req, res, next) => {
+//   let token = req.headers["authorization"];
+//   console.log(token);
+//   token = token.slice(7, token.length);
+//   if (token) {
+//     jwt.verify(token, config.key, (err, decoded) => {
+//       if (err) {
+//         return res.json({
+//           status: false,
+//           msg: "token is invalid",
+//         });
+//       } else {
+//         req.decoded = decoded;
+//         next();
+//       }
+//     });
+//   } else {
+//     return res.json({
+//       status: false,
+//       msg: "Token is not provided",
+//     });
+//   }
+// };
+
+// module.exports = {
+//   checkToken: checkToken,
+// };
+
+
 const config = require("./config");
 
 let checkToken = (req, res, next) => {
   let token = req.headers["authorization"];
   console.log(token);
-  token = token.slice(7, token.length);
-  if (token) {
-    jwt.verify(token, config.key, (err, decoded) => {
-      if (err) {
-        return res.json({
-          status: false,
-          msg: "token is invalid",
-        });
-      } else {
-        req.decoded = decoded;
-        next();
-      }
-    });
-  } else {
-    return res.json({
+  if (!token || !token.startsWith("Bearer ")) {
+    return res.status(401).json({
       status: false,
-      msg: "Token is not provided",
+      msg: "Token is not provided or invalid",
+    });
+  }
+  
+  token = token.slice(7); // Remove 'Bearer ' from the token string
+  try {
+    const decoded = jwtDecode(token);
+    req.decoded = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      status: false,
+      msg: "Token is invalid",
     });
   }
 };
